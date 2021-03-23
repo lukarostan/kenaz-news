c2a1c312b0f196867faf9fb7a5bc71a1e3c405ae
lukarostan <lukarostan@gmail.com>
2021-03-23 18:12:48 +0100

    added footer

---
+++ src/components/FooterNewsItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="footer-item">
+        <div class="footer-item-content varela">
+            <div class="footer-item-date">
+                <span>August 26, 2013</span>
+                <span>5</span>
+            </div>
+            <h3>Palestinians call of peace talks after clash</h3>
+        </div>
+        <img src="../assets/footer-item.png" alt="">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="sass" scoped>
+.footer-item:nth-of-type(3)
+    border-bottom: none
+.footer-item
+    display: flex
+    flex-direction: row
+    color: #ccc
+    padding: 20px 0 40px 0
+    border-bottom: 1px solid #333
+    .footer-item-date
+        color: #666
+        display: flex
+        justify-content: space-between
+        width: 90%
+    h3
+        max-width: 90%
+    img
+        border: 2px solid #fff
+
+</style>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="footer">
+        <div class="footer-background">
+            <footer>
+                <div class="logo-and-social bitter">
+                    <div class="logo">
+                        <img src="../assets/kenaz-logo-footer.png" alt="">
+                        <h2>Kenaz</h2>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus leo ante.</p>
+                    <img class="social" src="../assets/social.png" alt="">
+                </div>
+                <div class="newsletter bitter">
+                    <h3>Newsletter</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus leo ante.</p>
+                    <input class="varela" type="text" placeholder="Your mail">
+                    <button class="varela">Subscribe</button>
+                </div>
+                <div class="tags">
+                    <h3 class="bitter">Tags Widget</h3>
+                    <div class="tags-container varela">
+                        <span class="tag">lorem</span>
+                        <span class="tag">ipsum</span>
+                        <span class="tag">dolor</span>
+                        <span class="tag">sit</span>
+                        <span class="tag">amet</span>
+                        <span class="tag">lorem</span>
+                        <span class="tag">ipsum</span>
+                        <span class="tag">dolor</span>
+                        <span class="tag">sit</span>
+                        <span class="tag">amet</span>
+                    </div>
+                </div>
+                <div class="featured">
+                    <h3 class="bitter">Featured</h3>
+                    <FooterNewsItem />
+                    <FooterNewsItem />
+                    <FooterNewsItem />
+                </div>
+                <div class="random-posts">
+                    <h3 class="bitter">Random Posts</h3>
+                    <FooterNewsItem />
+                    <FooterNewsItem />
+                    <FooterNewsItem />
+                </div>
+                <div class="recently-viewed">
+                    <h3 class="bitter">Recently Viewed</h3>
+                    <FooterNewsItem />
+                    <FooterNewsItem />
+                    <FooterNewsItem />
+                </div>
+            </footer>
+            <div class="footer-bottom varela">
+                <div class="footer-bottom-content">
+                    <p >© 2013 - Kenaz Template - Proudly made at Plava tvornica Croatia</p>
+                    <div class="footer-bottom-nav">
+                        <a href="#">Typography</a>-
+                        <a href="#">Templates</a>-
+                        <a href="#">Contact Form</a>-
+                        <a href="#">404 Page</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import FooterNewsItem from './FooterNewsItem'
+export default {
+    components:{
+        FooterNewsItem
+    }
+}
+</script>
+<style lang="sass" scoped>
+.footer
+    width: 100%
+    .footer-background
+        border-top: 20px solid $purple-light
+        background: $black-primary
+        footer > *
+            padding: 20px
+            
+        footer
+            width: 70%
+            margin: 40px auto
+            display: grid
+            grid-template-columns: 1fr 1fr 1fr
+            
+            h3
+                color: #CCC
+            p
+                margin: 20px 0
+                color: #666
+                font-size: 14px
+                line-height: 20px
+            .logo-and-social
+                display: flex
+                flex-direction: column
+                .logo
+                    display: flex
+                    flex-direction: row
+                    h2
+                        color: #45B0E3
+                        align-self: center
+                        margin: 0 10px 
+                        font-weight: 400
+                .social
+                    width: 400px
+            .newsletter
+                input
+                    padding: 15px
+                    border: none
+                    background: $black-secondary
+                    color: white
+                    width: 70%
+                button
+                    padding: 15px
+                    background: $purple-dark
+                    border: none
+                    color: #fff
+            .tags
+                .tags-container
+                    margin: 20px 0
+                    display: flex
+                    flex-wrap: wrap
+                    .tag
+                        padding: 5px
+                        background: $black-secondary
+                        color: #666
+                        margin: 2px
+                        transition: all 0.15s
+                        
+                    .tag:hover
+                        background: $purple-dark
+                        color: white
+                        cursor: pointer
+            .featured
+            .random-posts
+            .recently-viewed
+    .footer-bottom
+        border-top: 1px solid #333
+        .footer-bottom-content
+            width: 70%
+            margin: auto
+            display: flex
+            justify-content: space-between
+            color: #999
+            padding: 20px 0 100px 0
+            
+            a
+                color: #999
+                margin: 5px
+            a:hover
+                text-decoration: underline
+</style>
