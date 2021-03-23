20873cfc4d988cccdfb605a9570a8c72702b95d3
lukarostan <lukarostan@gmail.com>
2021-03-23 18:10:55 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   <div id="app">
     <PageHeader />
     <router-view/>
+    <PageFooter />
   </div>
 </template>
 <script>
 import PageHeader from './components/PageHeader.vue'
+import PageFooter from './components/PageFooter.vue'
 export default {
   components:{
     PageHeader,
+    PageFooter
 
   },
   created(){
