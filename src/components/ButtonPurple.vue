849fa7e546b0d3a68f1177ddb938e6bd7322a519
lukarostan <lukarostan@gmail.com>
2021-03-24 20:31:01 +0100

    Create ButtonPurple.vue

---
+++ src/components/ButtonPurple.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="button-container">
+        <button :class="height">{{buttonLabel}}</button>
+    </div>
+</template>
+<script>
+export default {
+    props:['buttonLabel','height']
+
+}
+</script>
+<style lang="sass" scoped>
+.tall
+    padding: 15px 
+.short
+    padding: 5px 15px
+button
+    background: $purple-dark
+    border: none
+    color: #fff
+    transition: all 0.15s
+button:hover
+    background: $purple-light
+    cursor: pointer
+</style>
