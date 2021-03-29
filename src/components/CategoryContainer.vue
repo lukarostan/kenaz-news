56d23c0aba35f0f86e44966f90419c20a5826ceb
lukarostan <lukarostan@gmail.com>
2021-03-29 14:50:39 +0200

    edit category containers

--- src/components/CategoryContainer.vue
+++ src/components/CategoryContainer.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="category-container" :class="categoryContainerColor">
+    <div class="category-container" :class="[categoryContainerColor, categoryContainerSize]">
         <div class="category-title">
             <h2 class="bitter">{{ categoryContainerTitle }}</h2>
-            <slot name="control">
-                
-            </slot>
+            <a v-if="!showControls" href="#" class="varela">{{ $t('see-all') }}</a>
+            <div v-if="showControls" class="controls">
+                <div class="arrow" :class="controlsColor" id="right"></div>
+                <div class="arrow" :class="controlsColor" id="left"></div>
+            </div>
         </div>
         <div class="category-news-container" :class="categoryContainerDisplay">
             <slot>
@@ -13,7 +15,6 @@
     </div>
 </template>
 <script>
-/* import NewsItemMedium from './NewsItemMedium' */
 export default {
     components:{
         
@@ -21,7 +22,10 @@ export default {
     props:[
         'categoryContainerTitle',
         'categoryContainerColor',
-        'categoryContainerDisplay']
+        'categoryContainerDisplay',
+        'categoryContainerSize',
+        'showControls',
+        'controlsColor']
 }
 </script>
 <style lang="sass" scoped>
@@ -36,6 +40,9 @@ export default {
     .grid
         display: grid
         grid-template-columns: 1fr 1fr
+    //categoryContainerSize
+    .small
+        width: 48%
 
     
 //categoryContainerColor
@@ -45,8 +52,17 @@ export default {
     border-left: 12px solid #009688
 .red
     border-left: 12px solid #EE6151
-
-
+.yellow
+    border-left: 12px solid #FCC44D
+.brown
+    border-left: 12px solid #A99765
+//controlsColor
+.yellowControls
+    border-left: 6px solid #FCC44D
+    border-bottom: 6px solid #FCC44D
+.brownControls
+    border-left: 6px solid #A99765
+    border-bottom: 6px solid #A99765
 
 
 .category-container
@@ -58,4 +74,19 @@ export default {
         display: flex
         justify-content: space-between
         text-transform: capitalize
+        .controls
+            display: flex
+            padding: 10px 20px
+            .arrow
+                height: 20px
+                margin: 0 5px
+                width: 20px
+                border-radius: 4px
+                transition: all 0.15s ease-out
+            .arrow:hover
+                cursor: pointer
+            #right
+                transform: rotate(45deg)
+            #left
+                transform: rotate(225deg)
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,70 +4,103 @@
     <MainSlider />
     <div class="home">
       <div class="main">
-          <CategoryContainer 
-          :categoryContainerTitle=" $t('news') "
+        <CategoryContainer
+          :categoryContainerTitle="$t('news')"
           :categoryContainerColor="'blue'"
-          :categoryContainerDisplay="'row'">
-            <SeeAll />
-            <NewsItemMedium />
-            <NewsItemMedium />
-            <NewsItemMedium />
-          </CategoryContainer>
-          <CategoryContainer 
-          :categoryContainerTitle=" $t('sports') "
+          :categoryContainerDisplay="'row'"
+          :showControls="false"
+        >
+          <NewsItemMedium />
+          <NewsItemMedium />
+          <NewsItemMedium />
+        </CategoryContainer>
+        <CategoryContainer
+          :categoryContainerTitle="$t('sports')"
           :categoryContainerColor="'teal'"
-          :categoryContainerDisplay="'row'">
-            <NewsItemMedium />
-            <NewsItemMedium />
-            <NewsItemMedium />
-          </CategoryContainer>
-          <Banner />
-          <CategoryContainer 
-          :categoryContainerTitle=" $t('business') "
+          :categoryContainerDisplay="'row'"
+          :showControls="false"
+        >
+          <NewsItemMedium />
+          <NewsItemMedium />
+          <NewsItemMedium />
+        </CategoryContainer>
+        <Banner />
+        <CategoryContainer
+          :categoryContainerTitle="$t('business')"
           :categoryContainerColor="'red'"
-          :categoryContainerDisplay="'grid'">
-            <NewsItemMedium />
-            <NewsItemMedium />
-            <NewsItemMedium />
+          :categoryContainerDisplay="'grid'"
+          :showControls="false"
+        >
+          <NewsItemMedium />
+          <NewsItemMedium />
+          <NewsItemMedium />
+          <NewsItemMedium />
+        </CategoryContainer>
+        <Banner />
+        <CategoryContainer
+          :categoryContainerTitle="$t('news-carousel')"
+          :categoryContainerColor="'yellow'"
+          :categoryContainerDisplay="'grid'"
+          :showControls="true"
+          :controlsColor="'yellowControls'"
+        >
+          <NewsItemMedium />
+          <NewsItemMedium />
+        </CategoryContainer>
+        <div class="small-containers">
+          <CategoryContainer
+            :categoryContainerTitle="$t('editorials')"
+            :categoryContainerColor="'brown'"
+            :categoryContainerSize="'small'"
+            :showControls="true"
+            :controlsColor="'brownControls'"
+          >
             <NewsItemMedium />
           </CategoryContainer>
-          <Banner />
+          <CategoryContainer
+            :categoryContainerTitle="$t('local-news')"
+            :categoryContainerColor="'brown'"
+            :categoryContainerSize="'small'"
+            :showControls="true"
+            :controlsColor="'brownControls'"
+          >
+            <NewsItemMedium />
+          </CategoryContainer>
         </div>
-        <SideContent />
+      </div>
+      <SideContent />
     </div>
     <Banner />
-    <PictureSlider v-if="false"/>
+    <PictureSlider v-if="true" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Banner from '../components/Banner.vue'
-import MainSlider from '../components/MainSlider.vue'
-import CategoryContainer from '../components/CategoryContainer.vue'
-import SideContent from '../components/SideContent.vue'
-import NewsItemMedium from '../components/NewsItemMedium'
-import SeeAll from '../components/SeeAll'
-import PictureSlider from '../components/PictureSlider'
+import Banner from "../components/Banner.vue";
+import MainSlider from "../components/MainSlider.vue";
+import CategoryContainer from "../components/CategoryContainer.vue";
+import SideContent from "../components/SideContent.vue";
+import NewsItemMedium from "../components/NewsItemMedium";
+import PictureSlider from "../components/PictureSlider";
 
 export default {
-  name: 'Home',
-  components:{
+  name: "Home",
+  components: {
     Banner,
     MainSlider,
     CategoryContainer,
     SideContent,
     NewsItemMedium,
-    SeeAll,
-    PictureSlider
+    PictureSlider,
   },
-  watch:{
-    news(){
-      console.log(this.$store.state.news)
-      return this.$store.state.news
-    }
-  }
-}
+  watch: {
+    news() {
+      console.log(this.$store.state.news);
+      return this.$store.state.news;
+    },
+  },
+};
 </script>
 <style lang="sass" scoped>
 @media only screen and (max-width: 600px)
@@ -77,7 +110,7 @@ export default {
       flex-direction: column
       .side
         order: -1
-      
+
 @media only screen and (min-width: 601px) and (max-width: 1024px)
   .content
     width: 90%
@@ -98,6 +131,11 @@ export default {
     width: 65%
     .side
       width: 55%
+@media only screen and (min-width: 601px)
+  .small-containers
+      display: flex
+      flex-direction: row
+      justify-content: space-between
 
 .content
   margin: 10px auto
@@ -105,8 +143,4 @@ export default {
     width: 100%
     margin: 10px 0
     display: flex
-    
-    
-        
-
 </style>
