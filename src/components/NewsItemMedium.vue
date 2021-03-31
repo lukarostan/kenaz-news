1136bedf678cf910bb6ef2386c07cc2ee9f07a8e
lukarostan <lukarostan@gmail.com>
2021-03-31 12:52:31 +0200

    made ready for v-for and need to fix style

--- src/components/NewsItemMedium.vue
+++ src/components/NewsItemMedium.vue
@@ -1,29 +1,38 @@
 <template v-slot:control>
     <div class="medium varela">
-        <img src="../assets/medium-example.png" alt="">
+        <img :src="image" alt="">
         <div class="medium-content-date">
-            <span>August 26, 2013</span>
+            <span>{{date}}</span>
             <span>14</span>
         </div>
-        <h3>For Obama, MLK's shadow looms large ahead of speech</h3>
+        <h3>{{title}}</h3>
     </div>
 </template>
 <script>
 export default {
+    props:['title','image','date']
 }
 </script>
 <style lang="sass" scoped>
+@media only screen and (min-width: 601px)
+    .medium
+        
+
 .medium
-    width: 1/3
+    transition: all 0.5s
     padding: 15px 20px 15px 0
+    min-width: 40%
     img
-        width: 100%
+        max-width: 60%
+        max-height: 60%
     .medium-content-date
         display: flex
         justify-content: space-between
         font-size: 12px
+        width: 80%
     h3
         font-size: 18px
+        width: 70%
 
 
 </style>
