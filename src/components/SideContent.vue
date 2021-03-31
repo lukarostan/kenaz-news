95342e371f6a8fda7ba7186c4db06ba9a8492a44
lukarostan <lukarostan@gmail.com>
2021-03-31 12:50:56 +0200

    Code formatting

--- src/components/SideContent.vue
+++ src/components/SideContent.vue
@@ -8,28 +8,27 @@
   </div>
 </template>
 <script>
-import FeaturedNewsContainer from './FeaturedNewsContainer'
-import Banner from './Banner'
+import FeaturedNewsContainer from "./FeaturedNewsContainer";
+import Banner from "./Banner";
 export default {
-    components:{
-        FeaturedNewsContainer,
-        Banner
-    }
+  components: {
+    FeaturedNewsContainer,
+    Banner,
+  },
 };
 </script>
 <style lang="sass" scoped>
 @media only screen and (min-width: 600px)
   .side
     margin: 10px 0 10px 20px
-    
-
 
 .side
-      .banner-container
-        display: grid
-        margin-top: 20px
-        grid-template-columns: 40% 40%
-        justify-content: space-around
-        padding: 15px
-        background: #ddd
+  min-width: 30%
+  .banner-container
+    display: grid
+    margin-top: 20px
+    grid-template-columns: 40% 40%
+    justify-content: space-around
+    padding: 15px
+    background: #ddd
 </style>
